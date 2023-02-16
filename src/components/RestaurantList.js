35936e6f3a83d0490e65aefbc41ea627f71dd06d
nbisek <nadiabisek02@gmail.com>
2023-02-16 15:15:43 -0500

    Load restaurants upon first rendering RestaurantLit

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,7 @@
+import {useEffect} from 'react';
+export default function RestaurantList({loadRestaurants}) {
+  useEffect(() => {
+    loadRestaurants();
+  }, [loadRestaurants]);
+  return <div>RestaurantList</div>;
+}
--- src/components/RestuarantList.js
+++
@@ -1,3 +0,0 @@
-export default function RestaurantList() {
-  return <div>RestaurantList</div>;
-}
